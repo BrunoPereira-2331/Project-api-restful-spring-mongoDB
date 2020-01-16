0059e6be16d6a2338384a159b16d8ec89ad2e2c4
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-01-15 23:56:19 -0300

    Project Created :V

---
+++ src/main/java/com/bruno/ProjectApiRestful/ProjectApiRestfulSpringMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.ProjectApiRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiRestfulSpringMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiRestfulSpringMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/ProjectApiRestful/ProjectApiRestfulSpringMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.ProjectApiRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiRestfulSpringMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
