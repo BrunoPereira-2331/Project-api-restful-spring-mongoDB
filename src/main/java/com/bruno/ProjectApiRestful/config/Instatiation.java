5363038430de0e2f8030742037f124aa807def37
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-01-16 12:44:17 -0300

    entity post created

--- src/main/java/com/bruno/ProjectApiRestful/config/Instatiation.java
+++ src/main/java/com/bruno/ProjectApiRestful/config/Instatiation.java
@@ -1,12 +1,16 @@
 package com.bruno.ProjectApiRestful.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.bruno.ProjectApiRestful.domain.Post;
 import com.bruno.ProjectApiRestful.domain.User;
+import com.bruno.ProjectApiRestful.repository.PostRepository;
 import com.bruno.ProjectApiRestful.repository.UserRepository;
 
 @Configuration
@@ -14,14 +18,26 @@ public class Instatiation implements CommandLineRunner{
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));		
 		userRepository.deleteAll();
-		User maria = new User(null, "Mario red", "Mario@gmail.com");
+		postRepository.deleteAll();
+		User mario = new User(null, "Mario red", "Mario@gmail.com");
 		User alex = new User(null, "Luigi Green", "Luigi@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são paulo", mario);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hj", mario);
+		
+		userRepository.saveAll(Arrays.asList(mario, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/bruno/ProjectApiRestful/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.bruno.ProjectApiRestful.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bruno.ProjectApiRestful.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
