8de3eaed67aa662f06bfe571cf525f155ab0499b
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-01-17 11:13:43 -0300

    simple query using @query

--- src/main/java/com/bruno/ProjectApiRestful/repository/PostRepository.java
+++ src/main/java/com/bruno/ProjectApiRestful/repository/PostRepository.java
@@ -3,13 +3,17 @@ package com.bruno.ProjectApiRestful.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bruno.ProjectApiRestful.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	@Query("{ title: { $regex: ?0, $options: 'i' } }")
+	public List<Post> searchTitle(String text);
+	
 	public List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
