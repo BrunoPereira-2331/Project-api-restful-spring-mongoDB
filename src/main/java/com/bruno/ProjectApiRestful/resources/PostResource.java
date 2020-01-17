9b98bb9129a870379dc514c61146f21d955dc06f
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-01-17 11:05:39 -0300

    simple query using query methods

--- src/main/java/com/bruno/ProjectApiRestful/resources/PostResource.java
+++ src/main/java/com/bruno/ProjectApiRestful/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.bruno.ProjectApiRestful.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bruno.ProjectApiRestful.domain.Post;
+import com.bruno.ProjectApiRestful.resources.util.URL;
 import com.bruno.ProjectApiRestful.services.PostService;
 
 @RestController
@@ -22,6 +26,12 @@ public class PostResource {
 		Post obj = service.findById(id);
 		//ok() instantiate responseEntity
 		return ResponseEntity.ok().body(obj);
-		
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
