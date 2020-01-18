cd3956a8c996bdbe19f7feb6c008f0aef17b3ed1
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-01-18 07:41:24 -0300

    query with many criteria

--- src/main/java/com/bruno/ProjectApiRestful/repository/PostRepository.java
+++ src/main/java/com/bruno/ProjectApiRestful/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.bruno.ProjectApiRestful.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -12,8 +13,11 @@ import com.bruno.ProjectApiRestful.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	@Query("{ title: { $regex: ?0, $options: 'i' } }")
-	public List<Post> searchTitle(String text);
+	List<Post> searchTitle(String text);
 	
-	public List<Post> findByTitleContainingIgnoreCase(String text);
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+ 	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
+
--- src/main/java/com/bruno/ProjectApiRestful/resources/PostResource.java
+++ src/main/java/com/bruno/ProjectApiRestful/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.bruno.ProjectApiRestful.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +16,35 @@ import com.bruno.ProjectApiRestful.resources.util.URL;
 import com.bruno.ProjectApiRestful.services.PostService;
 
 @RestController
-@RequestMapping(value="/posts")
+@RequestMapping(value = "/posts")
 public class PostResource {
 
 	@Autowired
 	private PostService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
-		//ok() instantiate responseEntity
+		// ok() instantiate responseEntity
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
